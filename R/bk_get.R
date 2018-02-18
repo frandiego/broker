a32057747f12540fb17a0450f91ebc2bf3fd87e4
Fran Diego <franciscojosediegoacosta@gmail.com>
2018-02-18 20:45:31 +0100

    add bk_viz

--- R/bk_get.R
+++ R/bk_get.R
@@ -1,6 +1,4 @@
 bk_get <- function(symbol,get="stock.prices",complete_cases=TRUE,..){
-  tidyquant::tq_get(x=symbol,get=get,complete_cases=complete_cases) %>%
-    mutate(symbol=symbol) %>%
-      select(symbol,everything())
+  tq_get(x=symbol,get=get,complete_cases=complete_cases)
 }
 
---
+++ R/bk_viz.R
@@ -0,0 +1,3 @@
+bk_viz <- function(bk_data){
+  tk_xts(data=df,silent = T,date_var = date) %>% dygraph()
+}
